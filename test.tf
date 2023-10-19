6072f9b3ea0bb6223c06d9af746cba8a6ba496ac
richardortizsalinas <35937365+richardortizsalinas@users.noreply.github.com>
2023-10-19 15:45:42 -0500

    Add files via upload

---
+++ test.tf
@@ -0,0 +1,59 @@
+data "aws_ami" "ubuntu" {
+
+ most_recent = true
+
+
+
+
+
+ filter {
+
+   name   = "name"
+
+   values = ["ubuntu*"]
+
+ }
+
+}
+
+
+
+
+
+locals {
+
+ instances = {
+
+   instance1 = {
+
+     ami           = data.aws_ami.ubuntu.id
+
+     instance_type = "t3.medium"
+
+   }
+
+   instance2 = {
+
+     ami           = data.aws_ami.ubuntu.id
+
+     instance_type = "t3.micro"
+
+   }
+
+ }
+
+}
+
+
+
+
+
+resource "aws_instance" "this" {
+
+ for_each      = local.instances
+
+ ami           = each.value.ami
+
+ instance_type = each.value.instance_type
+
+}
